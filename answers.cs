1fd9739578aa081afb5f0f3e551a43fa3530e68f
Robel Tessema <robeltessema.h@gmail.com>
2018-11-25 01:57:26 +0300

    create the answer placeholders

---
+++ answers.cs
@@ -0,0 +1,32 @@
+//Question One
+//1-Sum of Biggest Neighbor 
+
+
+
+
+//Question Two
+//2-User Class 
+
+
+
+//Question Three
+//3-John the Robot 
+
+
+
+
+//Question Four
+//4-Alexa Settings 
+
+
+
+
+
+//Question Five
+//5- Method Signature  
+
+
+
+
+//Question Six
+//6- Construction Game
